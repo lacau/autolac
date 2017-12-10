80659f67d3094772f9198f88e207da243351f954
lacau <llacau@gmail.com>
2017-12-10 13:17:55 -0200

    Initial structure. Add gitignore.

---
+++ src/main/java/com/autolac/AutolacApplication.java
@@ -0,0 +1,12 @@
+package com.autolac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutolacApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AutolacApplication.class, args);
+  }
+}
