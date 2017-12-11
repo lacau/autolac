87c2c17e6102d84ce380c79a3f2816486b549d03
lacau <llacau@gmail.com>
2017-12-11 10:45:48 -0200

    Add h2 database config. Change data model.

--- src/main/java/com/autolac/server/AutolacApplication.java
+++ src/main/java/com/autolac/server/AutolacApplication.java
@@ -1,4 +1,4 @@
-package com.autolac;
+package com.autolac.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/autolac/server/AutolacApplicationTests.java
+++ src/test/java/com/autolac/server/AutolacApplicationTests.java
@@ -1,4 +1,4 @@
-package com.autolac;
+package com.autolac.server;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
