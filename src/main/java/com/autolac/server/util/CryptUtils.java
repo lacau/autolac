26bb58115938531e9dc886040237e3713cf6a067
lacau <llacau@gmail.com>
2017-12-11 23:51:50 -0200

    Add code patterns.

--- src/main/java/com/autolac/server/util/CryptUtils.java
+++ src/main/java/com/autolac/server/util/CryptUtils.java
@@ -16,24 +16,24 @@ public final class CryptUtils {
     throw new IllegalStateException("CryptUtils class shouldn't be instantiated!");
   }
 
-  public static String generateToken(Credential credential) {
-    StringBuilder sb = new StringBuilder();
+  public static String generateToken(final Credential credential) {
+    final StringBuilder sb = new StringBuilder();
     sb.append(credential.getLogin());
     sb.append(":");
     sb.append(credential.getPassword());
     sb.append(":");
     sb.append(System.currentTimeMillis());
 
-    byte[] bytes = encryptValue(sb.toString(), SHA_256);
+    final byte[] bytes = encryptValue(sb.toString(), SHA_256);
     return convertToHex(bytes);
   }
 
-  public static String generateSHA256Password(String password) {
+  public static String generateSHA256Password(final String password) {
     return convertToHex(encryptValue(password, SHA_256));
   }
 
-  private static String convertToHex(byte[] raw) {
-    StringBuilder sb = new StringBuilder();
+  private static String convertToHex(final byte[] raw) {
+    final StringBuilder sb = new StringBuilder();
 
     for (int i = 0; i < raw.length; i++) {
       sb.append(Integer.toString((raw[i] & 0xff) + 0x100, 16).substring(1));
@@ -42,15 +42,16 @@ public final class CryptUtils {
     return sb.toString();
   }
 
-  private static byte[] encryptValue(String value, String algorithm) {
-    MessageDigest md = null;
+  private static byte[] encryptValue(final String value, final String algorithm) {
+    MessageDigest md;
 
     try {
       md = MessageDigest.getInstance(algorithm);
+      return md.digest(value.getBytes());
     } catch (NoSuchAlgorithmException e) {
       e.printStackTrace();
     }
 
-    return md.digest(value.getBytes());
+    return null;
   }
 }
